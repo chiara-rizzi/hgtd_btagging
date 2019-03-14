e44f87ec1a769b4990407aab978627d1d96d539d
chiara-rizzi <chiara_rizzi@hotmail.it>
2019-03-14 11:49:58 +0100

    add plot_from_dict

--- ROC_py/launch.sh
+++ ROC_py/launch.sh
@@ -1,29 +1,29 @@
-python plot_ROC.py --tagger IP3D --largeEta 0 --twoTrk 1
+#python plot_ROC.py --tagger IP3D --largeEta 0 --twoTrk 1
 python plot_ROC.py --tagger IP3D --largeEta 1 --twoTrk 1
-python plot_ROC.py --tagger MV1 --largeEta 0 --twoTrk 1
+#python plot_ROC.py --tagger MV1 --largeEta 0 --twoTrk 1
 python plot_ROC.py --tagger MV1 --largeEta 1 --twoTrk 1
-python plot_ROC.py --tagger SV1 --largeEta 0 --twoTrk 1
+#python plot_ROC.py --tagger SV1 --largeEta 0 --twoTrk 1
 python plot_ROC.py --tagger SV1 --largeEta 1 --twoTrk 1
-python plot_ROC.py --tagger IP3D+SV1 --largeEta 0 --twoTrk 1
+#python plot_ROC.py --tagger IP3D+SV1 --largeEta 0 --twoTrk 1
 python plot_ROC.py --tagger IP3D+SV1 --largeEta 1 --twoTrk 1
 
-python plot_ROC.py --tagger IP3D --largeEta 0 --twoTrk 0
+#python plot_ROC.py --tagger IP3D --largeEta 0 --twoTrk 0
 python plot_ROC.py --tagger IP3D --largeEta 1 --twoTrk 0
-python plot_ROC.py --tagger MV1 --largeEta 0 --twoTrk 0
+#python plot_ROC.py --tagger MV1 --largeEta 0 --twoTrk 0
 python plot_ROC.py --tagger MV1 --largeEta 1 --twoTrk 0
-python plot_ROC.py --tagger SV1 --largeEta 0 --twoTrk 0
+#python plot_ROC.py --tagger SV1 --largeEta 0 --twoTrk 0
 python plot_ROC.py --tagger SV1 --largeEta 1 --twoTrk 0
-python plot_ROC.py --tagger IP3D+SV1 --largeEta 0 --twoTrk 0
+#python plot_ROC.py --tagger IP3D+SV1 --largeEta 0 --twoTrk 0
 python plot_ROC.py --tagger IP3D+SV1 --largeEta 1 --twoTrk 0
 
 
-python plot_ROC.py --tagger IP3D --largeEta -1 --twoTrk 0
-python plot_ROC.py --tagger MV1 --largeEta -1 --twoTrk 0
-python plot_ROC.py --tagger SV1 --largeEta -1 --twoTrk 0
-python plot_ROC.py --tagger IP3D+SV1 --largeEta -1 --twoTrk 0
+#python plot_ROC.py --tagger IP3D --largeEta -1 --twoTrk 0
+#python plot_ROC.py --tagger MV1 --largeEta -1 --twoTrk 0
+#python plot_ROC.py --tagger SV1 --largeEta -1 --twoTrk 0
+#python plot_ROC.py --tagger IP3D+SV1 --largeEta -1 --twoTrk 0
 
-python plot_ROC.py --tagger IP3D --largeEta -1 --twoTrk 1
-python plot_ROC.py --tagger MV1 --largeEta -1 --twoTrk 1
-python plot_ROC.py --tagger SV1 --largeEta -1 --twoTrk 1
-python plot_ROC.py --tagger IP3D+SV1 --largeEta -1 --twoTrk 1
+#python plot_ROC.py --tagger IP3D --largeEta -1 --twoTrk 1
+#python plot_ROC.py --tagger MV1 --largeEta -1 --twoTrk 1
+#python plot_ROC.py --tagger SV1 --largeEta -1 --twoTrk 1
+#python plot_ROC.py --tagger IP3D+SV1 --largeEta -1 --twoTrk 1
 
