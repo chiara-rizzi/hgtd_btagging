2dda51d60ba30f95ac19395bbc0aea9c990f34d5
chiara-rizzi <chiara_rizzi@hotmail.it>
2019-03-19 16:19:59 +0100

    eff_maps works

--- ROC_py/eff_map.py
+++ ROC_py/eff_map.py
@@ -13,20 +13,13 @@ print("Ciao Chiara!")
 
 parser = argparse.ArgumentParser(description='Make efficiency plots.')
 parser.add_argument('--tagger', type=str, default="SV1")
-parser.add_argument('--largeEta', type=int, default=0)
-parser.add_argument('--twoTrk', type=int, default=0)
-parser.add_argument('--bEff', type=int, default=1)
+parser.add_argument('--twoTrk', type=int, default=1)
 
 args = parser.parse_args()
 
 tagger=args.tagger
-largeEta=args.largeEta>0
-smallEta=args.largeEta==-1
 twoTrk = args.twoTrk>0
-bEff = args.bEff>0
 
-print("largeEta:",largeEta)
-print("smallEta:",smallEta)
 
 def getCut( tagger ):
   if tagger=="MV1":  return  0.945487725
@@ -50,7 +43,8 @@ def getVariable( tagger ):
   if tagger=="MV2c20": return "jet_mv2c20"
   if tagger=="MVb":    return "jet_mvb"
   if tagger=="IP3D":   return  "jet_ip3d_llr"
-  if tagger=="IP3D+SV1":  return  "jet_sv1ip3d"
+  if tagger=="IP3D+SV1old":  return  "jet_sv1ip3d"
+  if tagger=="IP3D+SV1":  return  "jet_my_sv1ip3d"
   if tagger=="SV1":       return "jet_sv1_llr" 
   if tagger=="JetFitter": return  "jet_jf_m"
   return "0"
@@ -68,9 +62,35 @@ def getVariableNtrk( tagger ):
   if tagger=="JetFitter": return  "jet_ip3d_ntrk"
   return "0"
 
+def print_tf2(eff, key, num):
+  tf2_string = "Double_t fitfun_mu200_{}_70_{}_{}(Double_t *val, Double_t *par)".format(tagger.replace('+','').lower(), num, key)
+  tf2_string+= "{ \n"
+  tf2_string += "Float_t pt = val[0]; \n"
+  tf2_string += "Float_t eta = val[1]; \n"
+  tf2_string += "Double_t eff =0; \n"
+  for index, row in eff.iterrows():
+    pt_low = index[1].left
+    pt_high = index[1].right
+    eta_low = index[0].left
+    eta_high = index[0].right
+    value = row["eff"]
+    tf2_string += 'if (pt > {} && pt <= {} && eta > {} && eta <= {})  eff = {}; \n'.format(pt_low, pt_high, eta_low, eta_high, value)
+  tf2_string += "\n"
+  tf2_string += "return eff; \n}\n\n\n"
+  return tf2_string
 
-#keys = ["ITK", "Initial", "Int1", "Int2", "Final"]
-keys = ["ITK"]
+def get_ratio(g, invert=False):
+  num = float(g.loc[g[getVariable(tagger)] > getCut(tagger), :].shape[0])
+  den = float(g.shape[0])
+  if invert:
+    num,den = den,num
+  try:
+    return num/den
+  except ZeroDivisionError:
+    return np.nan
+
+keys = ["ITK", "Initial", "Int1", "Int2", "Final"]
+#keys = ["ITK"]
 
 condition= {
     "ITK":{
@@ -111,28 +131,55 @@ condition= {
         }
     }
 
+def compute_ip3d_sv1(row):
+    if  row["jet_ip3d_pu"] >0 and row["jet_ip3d_pb"] >0:
+        if  row["jet_ip3d_pu"] != 1 or  row["jet_ip3d_pu"] != 1.e9:
+          IP3DPlusSV1w= math.log ( row["jet_ip3d_pb"] / row["jet_ip3d_pu"] ) # logarithm neperien
+    else:
+        IP3DPlusSV1w = 0
+    if row["jet_sv1_ntrkv"]>0: #//SV1 found.
+        if  row["jet_sv1_pu"] >0 and row["jet_sv1_pb"] >0 :
+           IP3DPlusSV1w += math.log ( row["jet_sv1_pb"] / row["jet_sv1_pu"] );
+    else:
+        IP3DPlusSV1w += -1.55 # log(row["jet_sv1_pb"]/row["jet_sv1_pu"])  = log((1-eff_b)/(1-eff_u)) from the endpoint of the SV1 ROC curve
+    return IP3DPlusSV1w
+
 
 def get_df(t):  
-    df = t.pandas.df(["jet_pt","jet_eta","eventnb","jet_LabDr_HadF", "jet_dRminToB", 
-                      "jet_dRminToC", "jet_dRminToT", "jet_isPU", "jet_truthMatch", "PVz", 
-                      "truth_PVz", getVariableNtrk(tagger), getVariable(tagger)])
+    if tagger == "IP3D+SV1":
+        var_needed_for_ip3d_sv1=["jet_sv1_pb",
+                                 "jet_sv1_pu",
+                                 "jet_sv1_ntrkv",
+                                 "jet_ip3d_pb",
+                                 "jet_ip3d_pu"]
+        df = t.pandas.df(["jet_pt","jet_eta","eventnb","jet_LabDr_HadF", "jet_dRminToB", 
+                          "jet_dRminToC", "jet_dRminToT", "jet_isPU", "jet_truthMatch", "PVz", 
+                          "truth_PVz", getVariableNtrk(tagger)]+var_needed_for_ip3d_sv1) # add all the variables needed to compute IP3D+SV1
+        df["jet_my_sv1ip3d"] = df.apply (lambda row: compute_ip3d_sv1(row), axis=1)
+    else:
+        df = t.pandas.df(["jet_pt","jet_eta","eventnb","jet_LabDr_HadF", "jet_dRminToB", 
+                          "jet_dRminToC", "jet_dRminToT", "jet_isPU", "jet_truthMatch", "PVz", 
+                          "truth_PVz", getVariableNtrk(tagger), getVariable(tagger)])
+
     df["jet_pt"] = df["jet_pt"]/1000.
     df=df.rename_axis(index=['entry', 'jet_pos'])
     df=df.reset_index(level=['jet_pos'])
     df = df.set_index(["eventnb","jet_pos"])    
-    CutBase=" jet_pt>20 & jet_truthMatch==1 & jet_isPU==0 & abs(PVz-truth_PVz)<0.1  & abs(jet_eta)<4"
-    if largeEta:
-      CutBase = CutBase+" & abs(jet_eta)>2.4"
-      print("add large eta requirement")
-    if smallEta:
-      CutBase = CutBase+" & abs(jet_eta)<2.4"
-      print("add small eta requirement")
+    CutBase=" jet_pt>20 & abs(jet_eta)<4 & (jet_truthMatch==1 & jet_isPU==0 & abs(PVz-truth_PVz)<0.1"
     CutBase = CutBase+" & ( (jet_LabDr_HadF!=4 & jet_LabDr_HadF!=5 & jet_LabDr_HadF!=15 & jet_dRminToB>0.8 & jet_dRminToC>0.8 & jet_dRminToT>0.8)"
-    CutBase = CutBase+" | (jet_LabDr_HadF==5))"
+    CutBase = CutBase+" | (jet_LabDr_HadF==5) | (jet_LabDr_HadF==4))) | jet_isPU>0"
+
+    df['flav'] = np.where(df['jet_isPU']>0, 'PU',
+                          np.where( df['jet_LabDr_HadF'] ==5, 'B',
+                                    np.where( df['jet_LabDr_HadF'] ==4, 'C', 'L'                                              
+                                              )
+                                    )
+                          )
 
     df = df.query(CutBase)
 
     print(df.shape)
+    #print(df)
     return df
 
 def filter_df(df1, ITKcomp=0, twoTrk=0, df2=0):
@@ -157,122 +204,33 @@ def input_arrays(df):
   return df.as_matrix(columns=['jet_LabDr_HadF']), df.as_matrix(columns=[getVariable(tagger)])
   
 
-gs = gridspec.GridSpec(2, 1,
-                       height_ratios=[3,1],
-                       hspace=0.05)
-ax1 = plt.subplot(gs[0])
-plt.title(tagger) #
-eff_all={}
-tpr_all={}
-fpr_all={}
+num_flav = dict()
+num_flav['L'] = '0'
+num_flav['C'] = '1'
+num_flav['B'] = '2'
+num_flav['PU'] = '3'
+
 for i,key in enumerate(keys):
     print(key)
     f = uproot.open(condition[key]["file"]) # open file
     t = f[condition[key]["tree_name"]] #
     df = get_df(t)
     if int(i)==0:
-      df_ref = df.copy()
+        df_ref = df.copy()
     else:
-      df = filter_df(df, 1, twoTrk, df_ref)
-
-    print("So far so good!")
-
+        df = filter_df(df, 1, twoTrk, df_ref)
     
-    df['jet_eta_bin'] = pd.cut(df['jet_eta'].abs(), bins=[0, 0.5, 1.0, 1.5, 2.0, 2.4, 2.8, 3.2, 4.0])
+    df['jet_eta_bin'] = pd.cut(df['jet_eta'].abs(), bins=[0, 0.5, 1.0, 1.5, 2.0, 2.4, 2.8, 3.1, 4.0])
     df['jet_pt_bin'] =  pd.cut(df['jet_pt'],        bins=[0, 20, 30, 40, 60, 85, 110, 150, 250])
-    
 
-    def get_ratio(g, invert=False):
-      num = float(g.loc[g[getVariable(tagger)] > getCut(tagger), :].shape[0])
-      den = float(g.shape[0])
-      if invert:
-        num,den = den,num
-      try:
-        return num/den
-      except ZeroDivisionError:
-        return np.nan
-
-    eff = df.loc[df["jet_LabDr_HadF"]==5].groupby(['jet_eta_bin','jet_pt_bin']).apply(get_ratio)
-
-    eff = eff.to_frame()
-    eff.columns = ['eff']
-    #entries_tagged = entries_tagged.to_frame()
-    #print(eff.to_string())
-
-    def print_tf2(eff):
-      tf2_string = "Double_t func(Double_t *val, Double_t *par) { \n"
-      tf2_string += "Float_t pt = val[0]; \n"
-      tf2_string += "Float_t eta = val[1]; \n"
-      tf2_string += "Double_t eff =0; \n"
-      for index, row in eff.iterrows():
-        pt_low = index[1].left
-        pt_high = index[1].right
-        eta_low = index[0].left
-        eta_high = index[0].right
-        value = row["eff"]
-        tf2_string += 'if (pt > {} && pt <= {} && eta > {} && eta <= {})  eff = {}; \n'.format(pt_low, pt_high, eta_low, eta_high, value)
-      tf2_string += "\n"
-      tf2_string += "return eff; \n}"
-
-      return tf2_string
-    
-    print(print_tf2(eff))
+    f= open('efficiency_{}_{}.C'.format(key, tagger.replace('+','').lower()),"w")
+    for flav in num_flav:
+        print(flav)
+        eff = df.loc[df["flav"]==flav].groupby(['jet_eta_bin','jet_pt_bin']).apply(get_ratio)
+        eff = eff.to_frame()
+        eff.columns = ['eff']
+        f.write(print_tf2(eff, key, num_flav[flav])) 
+    f.close()
 
 
-"""    
-plt.plot(x, y, label=condition[key]['label'], color=condition[key]['color'])
-plt.legend(loc='best') 
-plt.xlim([0.,4.])
-if bEff:
-  plt.ylim([0.,1.0])
-  plt.ylabel('b-jet efficiency')
-else:  
-  max_ljr=max(max(fpr_all.values(), key = lambda g: max(g)))
-  plt.ylim([0.,1.3*max_ljr])
-  plt.ylabel('light rejection')
-plt.xlabel('jet eta')
-plt.text(0.02, 0.25, 'ATLAS Simulation Internal', size='large',transform=ax1.transAxes)
-plt.text(0.02, 0.15, '$t\overline{t}$ simulation', size='medium',transform=ax1.transAxes)
-if largeEta:
-    plt.text(0.02, 0.10, 'jet p$_{T}>$ 20 GeV, $|\eta|>$2.4', size='medium',transform=ax1.transAxes)
-elif smallEta:
-  plt.text(0.02, 0.10, 'jet p$_{T}>$ 20 GeV, $|\eta|<$2.4', size='medium',transform=ax1.transAxes)
-else:
-    plt.text(0.02, 0.10, 'jet p$_{T}>$ 20 GeV', size='medium',transform=ax1.transAxes)
-plt.grid(True)
-
-ax2 = plt.subplot(gs[1])
-for i,key in enumerate(keys):
-    print('\n')
-    print(key)
-    y=fpr_all[key] # light rejection
-    x=tpr_all[key]
-    if i==0:
-      reference_ynew = y
-    #print('   reference_ynew: ',reference_ynew[0:20])
-    ljr_ratio = np.divide(y, reference_ynew)
-    #print('   ljr_ratio: ',ljr_ratio[0:20])
-    print(condition[key]['label'])
-    plt.plot(x, ljr_ratio, color=condition[key]['color'])
-
-plt.xlim([0.,4.])
-plt.ylim([0.8,3])
-plt.ylabel('Ratio to ITK')
-plt.xlabel('jet eta')
-
-plt.grid(True)
-
-if bEff:
-  name_plot = 'eff_'+getVariable(tagger)
-else:
-  name_plot = 'mistag_'+getVariable(tagger)
-if largeEta:
-  name_plot+='_largeEta'
-if smallEta:
-  name_plot+='_smallEta'
-if twoTrk:
-  name_plot+='_twoTrk'
-name_plot+='_trkEffselfTag'
-plt.savefig(name_plot+'.pdf')
-"""   
 
